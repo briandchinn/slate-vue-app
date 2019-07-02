0afc20a23b2b2e876a407237b59ffab496eda098
briandchinn <bchinn310@gmail.com>
2019-07-02 07:23:37 -0700

    adds template tags and show logic to apply and applicants sections of projects show

--- src/views/Projects/Show.vue
+++ src/views/Projects/Show.vue
@@ -3,7 +3,6 @@
     
     {{ project }}
     <h1>{{ message }}</h1>
-<!--     <h3>Posted By: {{ project.user.first_name }} {{ project.user.last_name }}</h3> -->
     <h3>Title: {{ project.title }}</h3>
     <h3>Description: {{ project.description }}</h3>
     <h3>Address: {{ project.address }}</h3>
@@ -11,7 +10,7 @@
     <h3>End Date: {{ project.end_date }}</h3>
     <h3>Number of Positions: {{ project.number_of_positions }}</h3>
     <h3>Posted: {{ relativeDate(project.created_at) }}</h3>
-    <!-- <router-link v-bind:to="'/users/' + project.user.id">by {{ project.user.first_name }} {{ project.user.last_name }}</router-link> -->
+    <router-link v-bind:to="'/users/' + project.user.id">by {{ project.user.first_name }} {{ project.user.last_name }}</router-link>
     <br>
     <br>
     <button>
@@ -20,35 +19,39 @@
     <button v-on:click="destroy(project)">Destroy Project</button>
     <hr>
 
-    <h1>Would You Like to Apply?</h1>
-    <form v-on:submit.prevent="submit()">
-      <div class="form-group">
-        <label for="note">Note</label>
-        <input type="text" class="form-control" id="note" placeholder="Send a message to Project Owner" v-model="newProjectNote">
-      </div>
-      <button type="submit" class="btn btn-success">Apply</button>
-    </form>
-    {{ message }}
-    <hr>
-
-    <h1>Applicants</h1>
-    <div v-for="application in project.applications">
-      <router-link v-bind:to="/users/ + application.user_id">{{ application.user.first_name }}</router-link>
-      <h5>Application ID: {{ application.id }} </h5>
-      <h5>Note: {{ application.note }} </h5>
-      <h5>Offer Status: {{ application.offered }} </h5>
-      <h5>Accepted Status: {{ application.accepted }} </h5>
-      <h5>Favorite: {{ application.favorite }} </h5>
-      <h5>User ID: {{ application.user.id }} </h5>
-      <h5>First Name:{{ application.user.first_name }} </h5>
-      <h5>Last Name: {{ application.user.last_name }} </h5>
-      <h5>Email: {{ application.user.email }} </h5>
-      <img v-bind:src="application.user.image" alt="user images" width="50"><br>
-      <button v-on:click="hire(application)">Hire Applicant</button>
-      <button v-on:click="favorite(application)">Favorite</button>
+    <template v-if="project.user.id != $parent.user_id">
+      <h1>Would You Like to Apply?</h1>
+      <form v-on:submit.prevent="submit()">
+        <div class="form-group">
+          <label for="note">Note</label>
+          <input type="text" class="form-control" id="note" placeholder="Send a message to Project Owner" v-model="newProjectNote">
+        </div>
+        <button type="submit" class="btn btn-success">Apply</button>
+      </form>
+      {{ message }}
       <hr>
-    </div>
+    </template>
 
+    <template v-if="project.user.id == $parent.user_id">
+      <h1>Applicants</h1>
+      <div v-for="application in project.applications">
+        <router-link v-bind:to="/users/ + application.user_id">{{ application.user.first_name }}</router-link>
+        <h5>Application ID: {{ application.id }} </h5>
+        <h5>Note: {{ application.note }} </h5>
+        <h5>Offer Status: {{ application.offered }} </h5>
+        <h5>Accepted Status: {{ application.accepted }} </h5>
+        <h5>Favorite: {{ application.favorite }} </h5>
+        <h5>User ID: {{ application.user.id }} </h5>
+        <h5>First Name:{{ application.user.first_name }} </h5>
+        <h5>Last Name: {{ application.user.last_name }} </h5>
+        <h5>Email: {{ application.user.email }} </h5>
+        <img v-bind:src="application.user.image" alt="user images" width="50"><br>
+        <button v-on:click="hire(application)">Hire Applicant</button>
+        <button v-on:click="favorite(application)">Favorite</button>
+        <hr>
+      </div>
+    </template>
+    
   </div>
 </template>
 
